679dec716b1e35f187687cae543f1e404bc987fb
Kai Wycik <84764980+kaiwycik@users.noreply.github.com>
2025-10-11 11:06:01 -0400

    altered default model grids

--- src/carve/grids.py
+++ src/carve/grids.py
@@ -30,10 +30,10 @@ def default_model_grids(
     return [
         (KMeans, {"n_clusters": ks}),
         (AgglomerativeClustering, {"n_clusters": ks, "linkage": ["ward", "complete", "average", "single"]}),
-        # (SpectralClusteringCARVE, {"n_clusters": ks, "gamma": gamma_quantiles(X)}),
-        # (SpectralClusteringCARVE, {"n_clusters": ks, "affinity": ['knn'], "n_neighbors": [5, 10, 15, 20]}),
         (SpectralClustering, {"n_clusters": ks, "gamma": gamma_quantiles(X)}),
         # (SpectralClustering, {"n_clusters": ks, "affinity": ['nearest_neighbors'], "n_neighbors": [5, 10, 15, 20]}),
+        # (SpectralClusteringCARVE, {"n_clusters": ks, "gamma": gamma_quantiles(X)}),
+        # (SpectralClusteringCARVE, {"n_clusters": ks, "affinity": ['knn'], "n_neighbors": [5, 10, 15, 20]}),
     ]
     
 def default_norm_options() -> List[PreprocSpec]:
