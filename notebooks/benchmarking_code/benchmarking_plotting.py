af83a43b2a7fdb3336f3e181b3e65b63af55a7a3
Kai Wycik <84764980+kaiwycik@users.noreply.github.com>
2026-02-28 09:18:49 -0500

    modified color pallette.

--- notebooks/benchmarking_code/benchmarking_plotting.py
+++ notebooks/benchmarking_code/benchmarking_plotting.py
@@ -22,8 +22,8 @@ import warnings
 
 # --- Setup and basic handlers ---
 cluster_pallette = [
-    "#FF1F5B", "#00CD6C", "#009ADE", 
-    "#AF58BA", "#F28522", "#A6761D", "#A0B1BA"
+    "#009ADE", "#00CD6C", 
+    "#FF1F5B", "#AF58BA", "#F28522", "#A6761D", "#A0B1BA"
 ]
 
 lines_pallette_contrastive_carve = [
